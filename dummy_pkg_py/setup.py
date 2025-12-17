099e25ee6dae7ba656f864306d244ebcc7d68756
Adrian Carauleanu <adrian.carauleanu@gmail.com>
2025-12-17 12:56:02 +0200

    Add initial implementation of dummy_pkg_py and first_ros_package
    - Created dummy_pkg_py with basic structure including LICENSE, package.xml, setup.py, and initial Python script.
    - Implemented a simple console script in dummy_node_py.py that prints a greeting.
    - Added test files for copyright, flake8, pep257, and xmllint checks.
    - Established first_ros_package with a CMakeLists.txt, LICENSE, and package.xml for ROS 2 integration.
    - Implemented a basic C++ node that prints "hello world" and runs indefinitely.
    - Introduced tutorial_interfaces package with message and service definitions for ROS 2.
    - Added Num.msg and Sphere.msg message types, and AddThreeInts.srv service definition.

---
+++ dummy_pkg_py/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'dummy_pkg_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Adrian Carauleanu',
+    maintainer_email='adrian.carauleanu@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'dummy_node_py = dummy_pkg_py.dummy_node_py:main'
+        ],
+    },
+)
---
+++ first_ros_package/src/first_package_node.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char ** argv)
+{
+  printf("hello world first_ros_package package\n");
+  while(true);
+}
